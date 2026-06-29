8223bb6155e87a3bc81890d3b0facce4bf335f81
Sridhar <sridhar6381192@gmail.com>
2026-06-29 17:02:31 +0530

    Fix Prisma 7 schema configuration

--- faculty-backend/prismaClient.js
+++ faculty-backend/prismaClient.js
@@ -1,3 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+  datasourceUrl: process.env.DATABASE_URL
+});
 module.exports = prisma;
